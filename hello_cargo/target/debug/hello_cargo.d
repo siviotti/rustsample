961530ba4c85ef9d202e16683b2e11001a1564f2
07311631769 <douglas.siviotti@serpro.gov.br>
2019-09-30 10:29:58 -0300

    remove target

--- hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/home/07311631769/git/rustsample/hello_cargo/target/debug/hello_cargo: /home/07311631769/git/rustsample/hello_cargo/src/main.rs
--- hello_cargo/target/release/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/home/07311631769/git/rustsample/hello_cargo/target/release/hello_cargo: /home/07311631769/git/rustsample/hello_cargo/src/main.rs
